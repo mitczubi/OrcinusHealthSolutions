c7f8093ef42b54532ff89b180f15d2d85698f13d
Mitchell <mitchellzubich@gmail.com>
2023-07-27 14:27:04 -0400

    updates

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 const isProduction = process.env.NODE_ENV === "production";
 
 const baseUrl = isProduction
-  ? "https://orcin-LoadB-EL1JCQ8FGK3G-c427a6cf923b8fa0.elb.us-east-1.amazonaws.com/api"
+  ? "https://orcin-LoadB-EL1JCQ8FGK3G-c427a6cf923b8fa0.elb.us-east-1.amazonaws/api"
   : "http://localhost:4000/api";
 
 export default baseUrl;
