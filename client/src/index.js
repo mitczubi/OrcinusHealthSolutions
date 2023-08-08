465d70b6aa6732cbeb1419f7ed5ac356065ce5c0
Mitchell <mitchellzubich@gmail.com>
2023-08-08 13:00:57 -0400

    Adding calendly support

--- client/src/index.js
+++ client/src/index.js
@@ -1,39 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Homepage from "./pages/Homepage";
-import CoachingPage from "./pages/CoachingPage";
-import About from "./pages/AboutPage";
-import ConsultingPage from "./pages/ConsultingPage";
-import ContactPage from "./pages/ContactPage";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Homepage />,
-  },
-  {
-    path: "/coaching",
-    element: <CoachingPage />,
-  },
-  {
-    path: "/consulting",
-    element: <ConsultingPage />,
-  },
-  {
-    path: "/About",
-    element: <About />,
-  },
-  {
-    path: "/contact",
-    element: <ContactPage />,
-  },
-]);
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
